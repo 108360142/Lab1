52a1ecc7fa0a064e372367f17549a4fee2b5bdd4
108360142 <young.chiu2012@gmail.com>
2021-10-20 00:48:22 +0800

    first

---
+++ test_p16/Project3/src/Project3.java
@@ -0,0 +1,12 @@
+
+public class Project3 {
+
+	public static void main(String[] args) 
+	{
+		 System.out.println("A");
+		 System.out.println("Åwªï¨Ï¥ÎJava!");
+		 System.out.println(123);
+
+	}
+
+}
